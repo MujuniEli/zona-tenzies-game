5acc5889e57bd6769c36288ea0e666c699fb1cf6
MujuniEli <elijahmujuni@gmail.com>
2023-01-14 12:08:23 +0300

    add Roll feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { useState } from 'react';
 import Die from "./components/Die"
+import { nanoid } from 'nanoid'
 import './App.css';
 
 
@@ -10,20 +11,34 @@ function App() {
   function allNewDice() {
     const randomNumbers = []
     for(let i = 0; i < 10; i++){
-      randomNumbers.push(Math.ceil(Math.random() * 6))
+      randomNumbers.push({value: Math.ceil(Math.random() * 6), isHeld: false, id: nanoid()})
     }
 
     return randomNumbers
   }
 
-  const newNumbers = dice.map(item => <Die value={item}/>)
-  
+  function rollDice(){
+      setDice(allNewDice())
+  }
+
+  function holdDice(id) {
+    setDice(prevDice => prevDice.map(item => {
+      return item.id === id ? {...item, isHeld: !item.isHeld} : item
+    }))
+  }
+
+  const newNumbers = dice.map(item => 
+  <Die key={item.id} value={item.value} isHeld={item.isHeld} holdDice={()=>holdDice(item.id)}/>
+      )
+
   return (
     <main>
+      <h1 className='title'>zona's Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {newNumbers}
       </div>
-      
+      <button className='dice-roll' onClick={rollDice}>Roll</button>
     </main>
   );
 }
