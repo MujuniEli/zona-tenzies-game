02a5000b0138460b67abb82a7fc475d94f662aff
MujuniEli <elijahmujuni@gmail.com>
2023-01-29 20:56:48 +0300

    dice faces plus roll count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Die from "./components/Die"
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
+import dice1 from './faces/dice1.png';
+import dice2 from './faces/dice2.png';
+import dice3 from './faces/dice3.png';
+import dice4 from './faces/dice4.png';
+import dice5 from './faces/dice5.png';
+import dice6 from './faces/dice6.png';
 import './App.css';
 
 
 function App() {
   const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+  const [count, setCount] = useState(0)
+
   
+  
+  useEffect(()=> {
+    const allHeld = dice.every(die =>  die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValues = dice.every(die => firstValue === die.value)
+
+    if(allHeld && allSameValues){
+      setTenzies(true)
+      
+    }
+  }, [dice])
+
+  function randFace() {
+    const diceFaces = [dice1, dice2, dice3, dice4, dice5, dice6]
+    const randomIndex = Math.floor(Math.random() * diceFaces.length)
+    const finalFace = diceFaces.map(face => face[randomIndex])
+    return finalFace
+  }
+
+  
+  function generateRandomNumbers() {
+    return {value: randFace(), isHeld: false, id: nanoid()}
+  }
+
+
   function allNewDice() {
     const randomNumbers = []
     for(let i = 0; i < 10; i++){
-      randomNumbers.push({value: Math.ceil(Math.random() * 6), isHeld: false, id: nanoid()})
+      randomNumbers.push(generateRandomNumbers())
     }
 
     return randomNumbers
   }
 
   function rollDice(){
-      setDice(allNewDice())
+   
+      if(!tenzies){
+        setDice(prevDice => prevDice.map(item => {
+          return item.isHeld ? item : generateRandomNumbers()
+        }))
+      } else {
+
+              setTenzies(false)
+              setDice(allNewDice())
+      }
+      setCount(count => count + 1) 
   }
+
 
   function holdDice(id) {
     setDice(prevDice => prevDice.map(item => {
@@ -33,12 +79,14 @@ function App() {
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className='title'>zona's Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {newNumbers}
       </div>
-      <button className='dice-roll' onClick={rollDice}>Roll</button>
+      <button className='dice-roll' onClick={rollDice}>{tenzies ? "New Game" : "Roll" }</button>
+      <p>Number of Rolls: {count}</p>
     </main>
   );
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,4 +1,5 @@
 
+
 export default function Die(props) {
 
     const styles = {
@@ -7,7 +8,7 @@ export default function Die(props) {
 
     return (
                <div className="die-face" style={styles} onClick={props.holdDice}>
-                        <h2 className="die-num">{props.value}</h2>
+                        {props.value}
                         
                </div> 
     )
