c3ad979889d28564c22538f1df4d8db6b811cdcd
MujuniEli <elijahmujuni@gmail.com>
2023-03-07 08:38:19 +0300

    start page changes

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,23 +3,40 @@ import React, { useState } from "react"
 export default function Start({displayGame}) {
 
     const [playerList, setPlayerList] = useState([])
-    const [player, setPlayer] = useState("")
+    const [playerName, setPlayerName] = useState("")
   
   
     function handleChange(event) {
-      setPlayer(event.target.value)
+      setPlayerName(event.target.value)
+    }
+
+    function addPlayer() { 
+        setPlayerList([...playerList, playerName])
     }
 
     return (
-        <div>
-            <h3>Add Your names below for a Unique experience</h3>
+        <div className="start-page">
+            <h3>Add Your names below to play</h3>
             <div className="player-name">
                 <input 
                 type="text"
                 placeholder="Add player name"
                 onChange={handleChange}
                 />
-                <button> + </button>
+                <button onClick={addPlayer}> Add Player </button>
+            </div>
+            <div className="player-list">
+                {playerList.map((player) => {
+                    return (
+                        <div className="players">
+                            <h3>
+                            {player}
+                        </h3>
+                        <button> remove player </button>
+                        </div>
+                        
+                    )
+                })}
             </div>
             
             <button type="button" className="start-btn" onClick={displayGame}>Start Game</button>
