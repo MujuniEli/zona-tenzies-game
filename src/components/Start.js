7ee12ef8ca2a299bdd77cb2296bcf9d73a9e6647
MujuniEli <elijahmujuni@gmail.com>
2023-03-18 14:56:52 +0300

    update functionality

--- src/components/Start.js
+++ src/components/Start.js
@@ -4,20 +4,29 @@ import { nanoid } from "nanoid"
 export default function Start({displayGame}) {
 
     const [playerList, setPlayerList] = useState([])
-    const [playerName, setPlayerName] = useState("")
+    const [player, setPlayer] = useState("")
   
   
-    function handleChange(event) {
-      setPlayerName(event.target.value)
+    function handleChange(e) {
+        setPlayer({
+            id: '',
+            playerName: e.target.value
+      })
     }
 
-    function addPlayer() { 
-        const player = {
-            id: nanoid()
+    const addPlayer = (e) => {
+        const new_player = {
+            id: nanoid(),
+            playerName: player.playerName
         }
-        setPlayerList([...playerList, playerName])
+        setPlayerList([...playerList, new_player])
     }
 
+    function deletePlayer(event) {
+        setPlayerList(playerList.filter((item) => item.id !== event.target.value))
+    }
+    
+    
     return (
         <div className="start-page">
             <p className="instructions">Add Your names below to play as a specific player or click <b>Start Game</b> to play anonymously!</p>
@@ -25,19 +34,25 @@ export default function Start({displayGame}) {
                 <input 
                 type="text"
                 placeholder="Add player name"
+                value={player.playerName}
                 onChange={handleChange}
                 className="form-input"
                 />
-                <button className="form-btn" onClick={addPlayer}> Add Player </button>
+                <button
+                    className="form-btn"
+                    onClick={addPlayer}
+                >
+                    Add Player
+                </button>
             </div>
             <div className="player-list">
-                {playerList.map((player) => {
+                {playerList.map((item) => {
                     return (
-                        <div className="players">
+                        <div key={item.id} className="players">
                             <p className="player-name">
-                            {player.charAt(0).toUpperCase() + player.slice(1)}
-                        </p>
-                        <button className="rm-btn"> - </button>
+                                {item.playerName}
+                            </p>
+                            <button value={item.id} onClick={deletePlayer} className="rm-btn"> x </button>
                         </div>
                         
                     )
