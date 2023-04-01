28b5cd3d3c48896211b34ef5ca324de4a2404bdc
MujuniEli <elijahmujuni@gmail.com>
2023-04-01 18:13:33 +0300

    context

--- src/components/ContextAPI.js
+++ src/components/ContextAPI.js
@@ -4,5 +4,23 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    
+    const [player, setPlayer] = useState({
+        playerName: "",
+        rollCount: 0
+    })
+    const [playerList, setPlayerList] = useState([])
+
+    return (
+        <AppContext.Provider
+        value={{
+            player,
+            playerList
+        }}
+        >
+            {props.children}
+        </AppContext.Provider>
+    )
+
 }
+
+export default AppContextProvider;
